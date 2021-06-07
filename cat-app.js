51c87fa3c03cdd1a7937bfbb5a2818e541689bea
Stalin-Raj <70088135+Stalin-Raj@users.noreply.github.com>
2021-06-07 20:43:09 +0530

    Add files via upload

---
+++ cat-app.js
@@ -0,0 +1,6 @@
+function catGenerator(){
+  var image=document.createElement('img');
+  image.src="https://cdn2.thecatapi.com/images/61f.gif";
+  image.style="box-shadow:0px 5px 10px;";
+  document.getElementById('catContainer').appendChild(image);
+}
